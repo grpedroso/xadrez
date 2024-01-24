4ee7aafe06b61580d1c4d680299d02646c2dbf34
Gabriel Pedroso <gabrielpedrosor@gmail.com>
2024-01-24 15:33:28 -0300

    Primeiros testes de movimentações

--- xadrez/Tela.cs
+++ xadrez/Tela.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using jogoxadrez;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,13 @@ namespace xadrez
               Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
+        }
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
         }
         public static void imprimirPeca(Peca peca)
         {
